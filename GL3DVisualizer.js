6833f4b1be153dfb97c92dda45bffbb6619be7d1
Shinmera <shinmera@tymoon.eu>
2015-05-31 18:22:35 +0200

    Rewrote system to use classes, implemented renderer and visualization separation.

---
+++ GL3DVisualizer.js
@@ -0,0 +1,183 @@
+var GL3DVisualizer = function(html, FOV, NEAR, FAR){
+    var self = this;
+    Visualizer.call(self, html);
+    
+    self.FOV = FOV || 25;
+    self.NEAR = NEAR || 0.1;
+    self.FAR = 1000000;
+
+    self.canvas = $("canvas", html);
+    self.initialized = false;
+    self.objects = {};
+    self.keys = {};
+    self.scene = new THREE.Scene();
+    self.camera = new THREE.PerspectiveCamera(FOV, 1, NEAR, FAR);
+    self.camera.up.set(0, 0, 1);
+    self.glrenderer = new THREE.WebGLRenderer({canvas: self.canvas[0], alpha: true, antialias: true});
+    self.controls = new THREE.OrbitControls(self.camera, self.canvas[0]);
+
+    self.key = function(key, set){
+        switch(arguments.length){
+        case 1:
+            return self.keys[key]; break;
+        case 2:
+            if(set) self.keys[key]=key; else delete self.keys[key]; break;
+        default:
+            throw new Error("Illegal argument count");
+        }
+    }
+    self.addToScene = function(object, name){
+        self.objects[name] = object;
+        self.scene.add(object);
+        return self.objects;
+    }
+
+    self.clearScene = function(){
+        $.each(self.objects, function(k, v) {
+            self.scene.remove(v);
+            delete self.objects[k];
+        });
+        return self.objects;
+    }
+
+    self.populateScene = function(points){
+        $.each(points, function(k, v){
+            self.addToScene(self.makePoint(v["X"], v["Y"], v["Z"], v["size"], v["color"]), k);
+        });
+        return self.objects;
+    }
+
+    self.updateScene = function(points){
+        $.each(points, function(k, v){
+            self.objects[k].position.set(v["X"], v["Y"], v["Z"]);
+        });
+        return self.objects;
+    }
+
+    self.updateCamera = function(){
+        self.controls.update();
+
+        var look = new THREE.Vector3(0, 0, -1).applyQuaternion(self.camera.quaternion);
+        if(self.key(87) || self.key(38)){ // W / <up>
+            self.camera.position.add(look.clone().setLength(0.03));
+        }
+        if(self.key(83) || self.key(40)){ // S / <down>
+            self.camera.position.sub(look.clone().setLength(0.03));
+        }
+        if(self.key(65) || self.key (37)){ // A / <left>
+            self.controls.panLeft(0.01);
+        }
+        if(self.key(68) || self.key (39)){ // D / <right>
+            self.controls.panLeft(-0.01);
+        }
+        if(self.key(32)){
+            if(key(16))
+                self.controls.panUp(-0.01);
+            else
+                self.controls.panUp(0.01);
+        }
+        return self.camera;
+    }
+
+    self.render = function(){
+        self.updateCamera();
+        self.glrenderer.render(self.scene, self.camera);
+        requestAnimationFrame(self.render);
+        return true;
+    }
+
+    self.initUI = function(){
+        // Resize handling
+        $(window).resize(function(){
+            width = self.canvas.parent().innerWidth();
+            height = self.canvas.parent().innerHeight()-self.canvas.position().top;
+            
+            self.camera.aspect = width/height;
+            self.camera.left = width/-2;
+            self.camera.right = width/2;
+            self.camera.top = height/2;
+            self.camera.bottom = height/-2;
+            self.camera.updateProjectionMatrix();
+            self.glrenderer.setSize(width, height);
+        });
+
+        self.canvas.focus();
+        self.canvas.keydown(function(ev){
+            self.key(ev.keyCode||ev.which, true);
+        }).keyup(function(ev){
+            self.key(ev.keyCode||ev.which, false);
+        }).click(function(ev){
+            this.focus();
+        });
+        
+        $(window).trigger("resize");
+    }
+
+    self.initScene = function(){
+        var grid = new THREE.GridHelper(1, 0.1);
+        grid.geometry.applyMatrix(new THREE.Matrix4().makeRotationX(self.toRadians(90)));
+        // avoid intersecting with axis.
+        grid.geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0,0,-0.0001));
+        self.scene.add(grid);
+        var axisHelper = new THREE.AxisHelper(0.1);
+        self.scene.add(axisHelper);
+        
+        self.camera.position.x = 1;
+        self.camera.position.y = 1;
+        self.camera.position.z = 1;
+        self.controls.damping = 0.2;
+        return self.scene;
+    }
+
+    // public interface
+    self.clear = function(){
+        self.initialized = false;
+        self.clearScene();
+        return true;
+    }
+    
+    self.show = function(data){
+        if(self.initialized){
+            self.updateScene(data);
+        }else{
+            self.populateScene(data);
+            self.initialized = true;
+        }
+        return data;
+    }
+
+    self.initUI();
+    self.initScene();
+    self.render();
+
+    return self;
+}
+
+GL3DVisualizer.prototype = Object.create(Visualizer.prototype);
+GL3DVisualizer.prototype.constructor = GL3DVisualizer;
+
+GL3DVisualizer.prototype.toRadians = function(deg){
+    return deg*(Math.PI/180);
+}
+
+GL3DVisualizer.prototype.toDegrees = function(rad){
+    return rad*(180/Math.PI);
+}
+
+GL3DVisualizer.prototype.makePoint = function(x, y, z, size, color){
+    x = x || 0;
+    y = y || 0;
+    z = z || 0;
+    size = size || 0.0075;
+    color = color || 0xFF0000;
+
+    var geometry = new THREE.BoxGeometry(size, size, size);
+    var material = new THREE.MeshBasicMaterial({color: color});
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    return mesh;
+}
+
+/*
+ *  Public interface
+ */
--- renderer.js
+++ renderer.js
@@ -1,297 +1,221 @@
-$(function(){
-    var FOV = 25;
-    var NEAR = 0.1;
-    var FAR = 1000000;
+/*
+ * Visualizer superclass
+ */
+var Visualizer = function(html){
+    var self = this;
+    self.html = html;
 
-    var frame = 0;
-    var frames = [];
-    var framesLoaded = 0;
-    var renderDuration = 0;
-    var resolution = parseInt($("#player").data("resolution"));
-    var canvas = $("canvas");
-    var player = $("#player");
-    var objects = {};
-    var keys = {};
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(FOV, 1, NEAR, FAR);
-    //THREE.OrthographicCamera(600/-2, 600/2, 400/2, 400/-2, NEAR, FAR);
-    camera.up.set(0, 0, 1);
-    var renderer = new THREE.WebGLRenderer({canvas: canvas[0], alpha: true, antialias: true});
-    var controls = new THREE.OrbitControls(camera, canvas[0]);
-
-    function log(message){
-        var args = $.extend([], arguments);
-        args.unshift("[Renderer]");
-        console.log.apply(console, args);
-        return null;
-    }
-
-    function toRadians(deg){
-        return deg*(Math.PI/180);
-    }
-
-    function toDegrees(rad){
-        return rad*(180/Math.PI);
-    }
-
-    function allocateArray(length){
-        var array = [];
-        for(var i=0; i<length; i++){
-            array[i]=null;
-        }
-        return array;
-    }
-
-    // Set up according to data
-    data = allocateArray(parseInt($("#player").data("frames")));
-
-    // Resize handling
-    $(window).resize(function(){
-        width = canvas.parent().innerWidth();
-        height = canvas.parent().innerHeight()-canvas.position().top;
-        
-        camera.aspect = width/height;
-        camera.left = width/-2;
-        camera.right = width/2;
-        camera.top = height/2;
-        camera.bottom = height/-2;
-        camera.updateProjectionMatrix();
-        renderer.setSize(width, height);
-    });
-
-    // Key tracking
-    function key(key,set){
-        switch(arguments.length){
-        case 1:
-            return keys[key]; break;
-        case 2:
-            if(set) keys[key]=key; else delete keys[key]; break;
-        default:
-            throw new Error("Illegal argument count");
-        }
-    }
-
-    canvas.focus();
-    canvas.keydown(function(ev){
-        key(ev.keyCode||ev.which, true);
-    }).keyup(function(ev){
-        key(ev.keyCode||ev.which, false);
-    }).click(function(ev){
-        this.focus();
-    });
-
-    function makePoint(x, y, z, size, color){
-        x = x || 0;
-        y = y || 0;
-        z = z || 0;
-        size = size || 0.0075;
-        color = color || 0xFF0000;
-
-        var geometry = new THREE.BoxGeometry(size, size, size);
-        var material = new THREE.MeshBasicMaterial({color: color});
-        var mesh = new THREE.Mesh(geometry, material);
-        mesh.position.set(x, y, z);
-        return mesh;
-    }
-
-    function addToScene(object, name){
-        objects[name] = object;
-        scene.add(object);
-        return object;
-    }
-
-    function clearScene(){
-        $.each(objects, function(k, v) {
-            scene.remove(v);
-            delete objects[k];
-        });
-        return objects;
-    }
-
-    function populateScene(points){
-        $.each(points, function(k, v){
-            addToScene(makePoint(v["X"], v["Y"], v["Z"], v["size"], v["color"]), k);
-        });
-    }
-
-    function updateScene(points){
-        $.each(points, function(k, v){
-            objects[k].position.set(v["X"], v["Y"], v["Z"]);
-        });
-    }
-
-    // Scene setup
-    function init(){
-        var grid = new THREE.GridHelper(1, 0.1);
-        grid.geometry.applyMatrix(new THREE.Matrix4().makeRotationX(toRadians(90)));
-        // avoid intersecting with axis.
-        grid.geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0,0,-0.0001));
-        scene.add(grid);
-        var axisHelper = new THREE.AxisHelper(0.1);
-        scene.add(axisHelper);
-        
-        camera.position.x = 1;
-        camera.position.y = 1;
-        camera.position.z = 1;
-        controls.damping = 0.2;
-        animate();
-        return scene;
-    }
-
-    // Main loop
-    function updateCamera(){
-        controls.update();
-
-        var look = new THREE.Vector3(0, 0, -1).applyQuaternion(camera.quaternion);
-        if(key(87) || key(38)){ // W / <up>
-            camera.position.add(look.clone().setLength(0.03));
-        }
-        if(key(83) || key(40)){ // S / <down>
-            camera.position.sub(look.clone().setLength(0.03));
-        }
-        if(key(65) || key (37)){ // A / <left>
-            controls.panLeft(0.01);
-        }
-        if(key(68) || key (39)){ // D / <right>
-            controls.panLeft(-0.01);
-        }
-        if(key(32)){
-            if(key(16))
-                controls.panUp(-0.01);
-            else
-                controls.panUp(0.01);
-        }
-        return camera;
+    self.clear = function(){
+        throw "Visualizer must implement the clear() method!";
     }
     
-    function animate(){
-        updateCamera();
-
-        if(isPlaying() && frame<framesLoaded-1){
-            // Calculate next frame by approximating time distance
-            // through previous time taken to render the frame.
-            var frameDistance = (renderDuration/1000)*resolution;
-            var newFrame = frame+Math.floor(frameDistance);
-            if(newFrame < framesLoaded){
-                setFrame(newFrame);
-            }else{
-                setFrame(framesLoaded-1);
-                setPlaying(false);
-            }
-        }
-        
-        return scene;
+    self.show = function(data){
+        throw "Visualizer must implement the show(data) method!";
     }
+    
+    return self;
+}
 
-    var lastFrame = 0;
-    function render(){
-        var now = performance.now();
-        if(0 < lastFrame)
-            renderDuration = now-lastFrame;
-        lastFrame = now;
-        renderer.render(animate(), camera);
-        requestAnimationFrame(render);
-        return null;
-    };
+/*
+ * Main Renderer class
+ */
+var Renderer = function(player, resolution, frames){
+    var self = this;
+    var actualPlayer = player
+        || null;
+    var actualResolution = resolution
+        || parseInt($(actualPlayer).data("resolution"))
+        || (self.log("Warn: No resolution given or found, defaulting to",30) && 30);
+    var actualFrames = frames
+        || parseInt($(actualPlayer).data("frames"))
+        || 0;
 
-    // Init
-    $(window).trigger("resize");
-    init();
-    render();
+    if(actualPlayer == null) throw "No player given to renderer!";
+    
+    self.player = $(actualPlayer);
+    self.resolution = actualResolution;
+    self.frame = 0;
+    self.frames = self.allocateArray(actualFrames);
+    self.framesLoaded = 0;
+    self.visualizers = {};
 
-    function updateDisplay(){
+    self.updateDisplay = function(){
         var pad = function(thing, count){
             thing = ""+thing;
             count = (count || 2) - thing.length;
             return (count<0)? thing : new Array(count+1).join("0")+thing;
         }
-        var displayUnit = $("#seeker", player).width()/frames.length;
-        $("#seeker #loaded", player).width(framesLoaded*displayUnit+"px");
-        $("#seeker #current", player).width(frame*displayUnit+"px");
-        var seconds = frame/resolution;
-        var msecs = frame%resolution/resolution*1000;
+        var displayUnit = $(".seeker", self.player).width()/self.frames.length;
+        $(".seeker .loaded", self.player).width(self.framesLoaded*displayUnit+"px");
+        $(".seeker .current", self.player).width(self.frame*displayUnit+"px");
+        var seconds = self.frame / self.resolution;
+        var msecs = self.frame % self.resolution / self.resolution * 1000;
         var secs = Math.floor(seconds) % 60;
         var mins = Math.floor(seconds / 60);
-        $("#frameinfo", player).text(pad(mins)+":"+pad(secs)+":"+pad(msecs,3));
+        var text = pad(mins)+":"+pad(secs)+":"+pad(msecs,3);
+        $(".frameinfo", self.player).text(text);
+        return text;
     }
 
-    function setFrame(newframe){
-        data = frames[newframe];
-        if(data){
-            frame = newframe;
-            updateScene(data);
-            updateDisplay();
+    self.frameData = function(frame){
+        var actframe = frame || self.frame;
+        return (actframe<self.framesLoaded && 0<=actframe) ? self.frames[actframe] : null;
+    }
+
+    self.setFrame = function(newframe){
+        var data = self.frameData(newframe);
+        if(data && newframe != self.frame){
+            self.frame = newframe;
+            self.updateDisplay();
+
+            $.each(self.visualizers, function(i, visualizer){
+                visualizer.show(data);
+            });
         }
-        return frame;
+        return self.frame;
     }
 
-    function setFrameData(data){
-        frames = data;
-        framesLoaded = data.length;
-        setFrame(0);
-        return frames;
+    self.setFrameData = function(data){
+        self.frames = data;
+        self.framesLoaded = data.length;
+        self.setFrame(0);
+        return self.frames;
     }
 
-    function addFrameData(data){
+    self.addFrameData = function(data){
         for(var i=0; i<data.length; i++){
-            frames[i+framesLoaded] = data[i];
+            self.frames[i+self.framesLoaded] = data[i];
         }
-        if(framesLoaded == 0 && data.length>0){
-            frame = 0;
-            populateScene(frames[frame])
+        if(self.framesLoaded == 0 && data.length>0){
+            self.frame = 0;
         }
-        framesLoaded += data.length;
-        updateDisplay();
-        return frames;
+        self.framesLoaded += data.length;
+        self.updateDisplay();
+        return self.frames;
     }
 
-    function setPlaying(val){
+    var playing = false;
+    self.setPlaying = function(val){
+        playing = val;
         if(val){
-            $("#play i", player).removeClass("icon-play")
-                .addClass("icon-pause");
-            if(frame == frames.length-1){
-                setFrame(0);
+            self.log("Playing");
+            $(".play i", self.player).removeClass("icon-play").addClass("icon-pause");
+            if(self.frame == self.frames.length-1){
+                self.setFrame(0);
             }
         }else{
-            $("#play i", player).addClass("icon-play")
-                .removeClass("icon-pause");
+            self.log("Pausing");
+            $(".play i", self.player).addClass("icon-play").removeClass("icon-pause");
         }
+        return playing;
     }
 
-    function isPlaying(){
-        return $("#play i", player).hasClass("icon-pause");
+    self.isPlaying = function(){
+        return playing;
     }
 
-    $("#play", player).click(function(){
-        setPlaying(!isPlaying());
-    });
+    self.seekerFrame = function(ev){
+        var seeker = $(".seeker", self.player);
+        var x = ev.pageX - seeker.offset().left;
+        return Math.floor(x/seeker.width()*self.frames.length);
+    }
 
-    var wasplaying = false;
-    var isholding = false;
-    $("#seeker", player).mousedown(function(ev){
-        isholding = true;
-        wasplaying = isPlaying();
-        setPlaying(false);
-        var x = ev.pageX - $(this).offset().left;
-        setFrame(Math.floor(x/$(this).width()*frames.length));
-    });
-    $(window).mousemove(function(ev){
-        if(isholding){
-            var seeker = $("#seeker", player);
-            var x = ev.pageX - seeker.offset().left;
-            setFrame(Math.floor(x/seeker.width()*frames.length));
-        }
-    }).mouseup(function(ev){
-        if(isholding){
-            isholding = false;   
-            setPlaying(wasplaying);
-        }
-    });
+    self.loadVisualizer = function(js, html, css){
+        var funct = new Function(js);
+        var name = funct();
+        var style = $("<style type=\"text/css\" />").data("visualizer", name).html(css||"");
+        var html = $($.parseHTML(html)).data("visualizer",name);
+        
+        $("head").append(style);
+        $(".visualizations",self.player).append(html);
+        self.visualizers[name] = new window[name]();
+        return self.visualizers[name];
+    }
 
-    // Load proper frame data
-    log("Loading testdata...");
+    self.initUI = function(){
+        $(".play", self.player).click(function(){
+            self.setPlaying(!self.isPlaying());
+        });
+        
+        var wasplaying = false;
+        var isholding = false;
+        $(".seeker", self.player).mousedown(function(ev){
+            isholding = true;
+            wasplaying = self.isPlaying();
+            self.setPlaying(false);
+            self.setFrame(self.seekerFrame(ev));
+        });
+        
+        $(window).mousemove(function(ev){
+            if(isholding){
+                self.setFrame(self.seekerFrame(ev));
+            }
+        }).mouseup(function(ev){
+            if(isholding){
+                isholding = false;   
+                self.setPlaying(wasplaying);
+            }
+        });
+        return true;
+    }
+
+    var frameDuration = 0;
+    var lastFrameTime = 0;
+    self.step = function(){
+        var now = performance.now();
+        if(0 < lastFrameTime)
+            frameDuration = now-lastFrameTime;
+        lastFrameTime = now;
+
+        if(self.isPlaying() && self.frame<self.framesLoaded-1){
+            // Calculate next frame by approximating time distance
+            // through previous time taken to render the frame.
+            var frameDistance = (frameDuration/1000)*self.resolution;
+            var newFrame = self.frame+Math.floor(frameDistance);
+            if(newFrame < self.framesLoaded){
+                self.setFrame(newFrame);
+            }else{
+                self.setFrame(self.framesLoaded-1);
+                self.setPlaying(false);
+            }
+        }
+
+        window.requestAnimationFrame(self.step);
+        return frameDuration;
+    };
+
+    self.initUI();
+    self.step();
+
+    return self;
+}
+
+Renderer.prototype.log = function(message){
+    var args = $.extend([], arguments);
+    args.unshift("[Renderer]");
+    console.log.apply(console, args);
+    return true;
+}
+
+Renderer.prototype.allocateArray = function(length){
+    var array = [];
+    for(var i=0; i<length; i++){
+        array[i]=null;
+    }
+    return array;
+}
+
+var renderer = null;
+
+$(function(){
+    renderer = new Renderer($(".player").first());
+
+    renderer.loadVisualizer('return "GL3DVisualizer";', '<canvas tabindex="0">HTML5 Canvas &amp; WebGL support required.</canvas>');
+
+    // // Load proper frame data
+    renderer.log("Loading testdata...");
     jQuery.getJSON("testdata.json", function(data){
-        addFrameData(data);
-        log("Done loading.");
+        renderer.addFrameData(data);
+        renderer.log("Done loading.");
     });
 })
