9c78375929f96478c93d5bb2ab8ce19b2c2ea0b4
Shinmera <shinmera@tymoon.eu>
2015-06-21 23:28:44 +0200

    Automatic opening/closing of sources

--- config.lisp
+++ config.lisp
@@ -7,9 +7,13 @@ Author: Nicolas Hafner <shinmera@tymoon.eu>
 
 (defun start ()
   (start-listener 8080)
-  )
+  (dolist (file (uiop:directory-files (resource "source/")))
+    (when (string-equal (pathname-type file) "tide")
+      (open-source :file file))))
 
-(defun stop ()
+(defun stop (&key quit)
   (stop-listener 8080)
   (dolist (source (list-sources))
-    (close-source source)))
+    (close-source source))
+  (when quit
+    (uiop:quit)))
